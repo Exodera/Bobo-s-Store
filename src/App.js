503fc7bad04caf8e4ea934e0b35a987d4a318c6a
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-10-03 23:12:40 +0200

    created pages & imported basic packages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from './pages/Home';
+import Drinks from './pages/Drinks';
+import Food from './pages/Food';
+import CallUs from './pages/CallUs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element = {<Home/>}/>
+          <Route path='/food' element = {<Food/>}/>
+          <Route path='/drinks' element = {<Drinks/>}/>
+          <Route path='/call-us' element = {<CallUs/>}/>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <span className='logo'>Bobo's Store</span>
+        <div className='links'>
+            <Link to = "#">Food</Link>
+            <Link to = "#">Drinks</Link>
+            
+
+
+        </div>
+
+    </div>
   )
 }
 
