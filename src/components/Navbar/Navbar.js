5a0c1c74e6d6909bce9ec32e57222e85f0f7d183
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-10-03 23:37:05 +0200

    ui mockup update and web changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import "./Navbar.css"
 
 
 const Navbar = () => {
@@ -7,11 +8,9 @@ const Navbar = () => {
     <div className='navbar'>
         <span className='logo'>Bobo's Store</span>
         <div className='links'>
-            <Link to = "#">Food</Link>
-            <Link to = "#">Drinks</Link>
-            
-
-
+            <Link to = "/food">Food</Link>
+            <Link to = "/drinks">Drinks</Link>
+            <Link to = "/callUs">Call Us</Link>
         </div>
 
     </div>
