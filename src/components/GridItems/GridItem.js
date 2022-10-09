64960d9f333ee87c9567f755a32f6998313c0d5d
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-10-09 13:45:04 +0200

    added Images

--- src/components/GridItems/GridItem.js
+++ src/components/GridItems/GridItem.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./GridItem.css"
+import picSaskoBrown from "../images/sasko-brown.jpg"
 
 
 const GridItem = ({props}) => {
   return (
     <div>
       <div className='item'>
-        <img classname = "item-image" src='' alt = "item icon"/>
+        <img classname = "item-image" src={picSaskoBrown} alt = "item icon"/>
         <h4 className = "item-name">{props.Name}</h4>
         <h3 className = "item-price">{props.Price}</h3>
         
